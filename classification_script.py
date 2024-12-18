32f4e2ef4718fd7c7b0febed1fd3105390c21ccb
pichlerd21 <pichlerd21@unet.univie.ac.at>
2024-12-18 12:05:59 +0100

    Add python script

---
+++ classification_script.py
@@ -0,0 +1,198 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import pandas as pd
+import csv
+
+#Pytorch
+import torch
+import torchvision
+from torchvision import datasets, transforms, models
+import torch.utils.data
+import torch.utils.data.dataloader
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, random_split
+from PIL import Image
+
+# Evaluation
+from sklearn.metrics import f1_score, precision_score, recall_score, confusion_matrix
+from itertools import groupby
+
+FOLDERPATH = '/Users/davidpichler/GitHubRepo/Thesis/data/segmented/graph/spec'
+
+# Validation function
+def validate(model, device, loader):
+    correct = 0
+    total = 0
+    
+    model.eval()
+    with torch.no_grad():
+        for images, labels in loader:
+            # Send data to device
+            images, labels = images.to(device), labels.to(device)
+
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+        
+    return correct / total
+
+
+def train(model, device):
+    model = model.to(device)
+
+    # Transofrm the images
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+    ])
+
+
+    # Load the dataset
+    dataset = datasets.ImageFolder(FOLDERPATH, transform=transform)
+
+    random_seed = 2024
+    torch.manual_seed(random_seed)
+
+    # Define the sizes of the train, validate and test sets
+    train_size = int(0.8 * len(dataset))
+    val_size = int(0.1 * len(dataset))
+    test_size = len(dataset) - train_size - val_size
+
+    # Split the dataset into train, validate and test sets
+    train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, val_size, test_size])
+    print('Train Dataset: ' + str(len(train_dataset)))
+    print('Validation Dataset: ' + str(len(val_dataset)))
+    print('Test Dataset: ' + str(len(test_dataset)))
+
+
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=4)
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=32, shuffle=True, num_workers=4)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=32, shuffle=True, num_workers=4)
+
+    # 6. Verlustfunktion und Optimierer definieren
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    num_epochs = 20
+
+    acc = []
+    loss_values = []
+
+    with open('results/train.csv', 'w', newline='') as csvfile:
+        fieldnames = ['Epoch', 'Loss', 'Validation Accuracy']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        # Train the model
+        for epoch in range(num_epochs):
+            model.train()
+            for inputs, labels in train_loader:
+                inputs, labels = inputs.to(device), labels.to(device)
+                optimizer.zero_grad()
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
+            
+            train_accuracy = validate(model, device, val_loader)
+            
+            loss_values.append(loss.item())
+            acc.append(train_accuracy)
+
+            print(f'Epoch: {epoch + 1} | Loss: {loss.item()} | Validation Accuracy:  {train_accuracy}')
+
+            # Schreiben der Ergebnisse in die CSV-Datei
+            writer.writerow({
+                'Epoch': epoch + 1,
+                'Loss': loss.item(),
+                'Validation Accuracy': train_accuracy
+            })
+
+    test(model, device, test_loader)
+
+
+def test(model, device, data_loader):
+    # Test the model
+    model.eval()
+
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        true_labels = []
+        predicted_labels = []
+
+        for X_test_tensor, Y_test_tensor in data_loader:
+            X_test_tensor, Y_test_tensor = X_test_tensor.to(device), Y_test_tensor.to(device)
+            outputs = model(X_test_tensor)
+            _, predicted = torch.max(outputs.data, 1)
+
+            y_test = Y_test_tensor.cpu().numpy()
+            predicted = predicted.cpu().numpy()
+
+            correct += (predicted == y_test).sum().item()
+            total += len(y_test)
+
+            true_labels.extend(y_test)
+            predicted_labels.extend(predicted)
+
+        accuracy = correct / total
+        print(f'Accuracy: {accuracy}')
+
+        f1 = f1_score(true_labels, predicted_labels, average='weighted')
+        print(f'F1 Score: {f1}')
+
+        precision = precision_score(true_labels, predicted_labels, average='weighted')
+        print(f'Precision: {precision}')
+
+        recall = recall_score(true_labels, predicted_labels, average='weighted')
+        print(f'Recall: {recall}')
+
+    # Confusion Matrix
+    # Create a DataFrame from the confusion matrix
+    cm = confusion_matrix(true_labels, predicted_labels)
+    cm_df = pd.DataFrame(cm, index=['Actual Corrected', 'Actual Original', 'Actual Smoothed'], columns=['Predicted Corrected', 'Predicted Original', 'Predicted Smoothed'])
+
+    # Display the DataFrame
+    print(cm_df)
+
+    with open('results/test_data.txt', 'a') as f:
+        f.write(f'Accuracy: {accuracy}\n')
+        f.write(f'F1 Score: {f1}\n')
+        f.write(f'Precision: {precision}\n')
+        f.write(f'Recall: {recall}\n')
+        f.write(f'Confusion Matrix: {cm_df}\n')
+
+def main():
+    # Check GPU
+    '''print(f"PyTorch version: {torch.__version__}")
+
+    print(f"Is CUDA available? {torch.cuda.is_available()}")
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f"Using device: {device}")'''
+
+    print(f"Is MPS built? {torch.backends.mps.is_built()}")
+    print(f"Is MPS available? {torch.backends.mps.is_available()}")
+
+    # Set device
+    device = "mps" if torch.backends.mps.is_available() else "cpu"
+    print(f"Using device: {device}")
+
+    # CNN Architectures
+    models_dict = {
+        "resnet50": models.resnet50(),
+        "regnet_y_3_2gf": models.regnet_y_3_2gf(),
+        "vgg16": models.vgg16()
+    }
+
+    # Train
+    train(models.resnet50(), device)
+
+
+
+if __name__=='__main__':
+    main()
+    
